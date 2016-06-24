577daa90a7b70503d093070280d5d055f16a8e3e
Artemiy <temitrix@gmail.com>
2016-06-24 14:55:54 +0300

    Initial Commit

---
+++ ExcurtionApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExcurtionApp
+//
+//  Created by Артмеий Шлесберг on 24/06/16.
+//  Copyright © 2016 Shlesberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
